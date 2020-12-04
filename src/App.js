d547caeb985a46dfdadd056dbe5e39f644d09c85
web1mhz <web1mhz@gmail.com>
2020-12-04 13:53:42 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex  flex-center">
+
+      <header className="bg_color flex flex-ji-c">
+
+      <p className="padding-lr">이제 시작하자! </p>  
+      <p className="padding-lr">시작하자!</p>  
+      <p className="padding-lr">리액트 프로젝트를</p> 
+        
+        
       </header>
     </div>
   );
